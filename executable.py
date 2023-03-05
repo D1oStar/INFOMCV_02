b5594bfca8b60d236fde1d90fdd317efb4fc9344
Sartinw <wsp981209@sina.com>
2023-03-05 16:02:07 +0100

    no change

--- executable.py
+++ executable.py
@@ -9,6 +9,12 @@ from engine.effect.bloom import Bloom
 from assignment import set_voxel_positions, generate_grid, get_cam_positions, get_cam_rotation_matrices
 from engine.camera import Camera
 from engine.config import config
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+from skimage import measure
+from skimage.draw import ellipsoid
 
 cube, hdrbuffer, blurbuffer, lastPosX, lastPosY = None, None, None, None, None
 firstTime = True
@@ -181,6 +187,7 @@ def key_callback(window, key, scancode, action, mods):
         global cube
         positions = set_voxel_positions(config['world_width'], config['world_height'], config['world_width'])
         cube.set_multiple_positions(positions)
+
 
 
 def mouse_move(win, pos_x, pos_y):
